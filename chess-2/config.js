c24f85b679b8488754b04b9bc1ecaad5e54c39ed
Natsion <123856194+Natsion@users.noreply.github.com>
2024-11-05 15:57:50 -0600

    Delete chess-2 directory

--- chess-2/config.js
+++
@@ -1,5 +0,0 @@
-// Export an object containing the server configuration settings
-module.exports = {
-    // Define the port number on which the server will run
-    port: 3037
-};
--- chess-2/index.js
+++
@@ -1,8 +0,0 @@
-// Load environment variables from a .env file
-require('dotenv').config();
-
-// Import the path module to work with file and directory paths
-const path = require('path');
-
-// Start the server by requiring the server.js file
-require(path.join(__dirname, 'server', 'server.js'));
